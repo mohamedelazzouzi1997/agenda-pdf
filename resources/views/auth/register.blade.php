7f3388c5c608a15e2d27df25c32e259b20e169da
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-02-05 14:52:46 +0100

    init

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.guest')
+@section('title', 'Register')
+@section('content')
+<div class="row">
+    <div class="col-lg-4 col-sm-12">
+        <form class="card auth_form" action="{{ route('register') }}" method="post">
+            @csrf
+            <div class="header mx-auto text-center">
+                <img class="logo mx-auto" src="{{asset('assets/images/logo.svg')}}" alt="">
+                <h5>Register</h5>
+                {{-- <span class="text-red-500">Register a new </span> --}}
+            </div>
+            <div class="body">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="name" placeholder="Username">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" name="email" placeholder="Enter Email">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="zmdi zmdi-email"></i></span>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" name="password_confirmation" placeholder="Password confirmation">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
+                    </div>
+                </div>
+                {{-- <div class="checkbox">
+                    <input id="remember_me" type="checkbox">
+                    <label for="remember_me">I read and agree to the <a href="javascript:void(0);">terms of usage</a></label>
+                </div> --}}
+                <button type="submit"  class="btn btn-primary btn-block waves-effect waves-light">REGISTER</button>
+                <div class="signin_with mt-3">
+                    {{-- <a class="link" href="{{route('authentication.login')}}">You already have a membership?</a> --}}
+                </div>
+            </div>
+        </form>
+        <div class="copyright text-center">
+            &copy;
+            <script>document.write(new Date().getFullYear())</script>,
+            <span>Designed by <a href="#" target="_blank">######</a></span>
+        </div>
+    </div>
+    <div class="col-lg-8 col-sm-12">
+        <div class="card">
+            <img src="{{asset('assets/images/signup.svg')}}" alt="Sign Up" />
+        </div>
+    </div>
+</div>
+@stop
+{{-- <x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <x-jet-label for="terms">
+                        <div class="flex items-center">
+                            <x-jet-checkbox name="terms" id="terms" required />
+
+                            <div class="ml-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </x-jet-label>
+                </div>
+            @endif
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-jet-button class="ml-4">
+                    {{ __('Register') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout> --}}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('after-styles')
+
+@endsection
+
+@section('content')
+<div class="text-red-500">
+    teyydk
+</div>
+@endsection
+
+
+@section('after-scripts')
+
+@endsection
